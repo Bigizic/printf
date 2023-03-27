ca6f64531318e68a61b7842abf3e6c7480ab4c95
Bigizic <olalekanisaac75@gmail.com>
2023-03-27 12:08:20 -0700

    get precision

--- get_precision.c
+++ get_precision.c
@@ -17,25 +17,32 @@ int get_precision(const char *format, int *i, va_list list)
 		return (precision);
 
 	precision = 0;
-
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+/* check the next character after '.' to see if it's a digit or '*', return -1 as the precision */
+	curr_i++;
+	if (format[curr_i] == '*')
 	{
-		if (is_digit(format[curr_i]))
-		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
-			break;
+		curr_i++;
+		precision = va_arg(list, int);
 	}
+	else if (isdigit(format[curr_i]))
+	{
+		while (isdigit(format[curr_i]))
+		{
+			precision = precision * 10 + (format[curr_i] - '0');
+			curr_i++;
+		}
+	}
+		else
+		{
+			return (-1);
+		}
+		*i = curr_i - 1;
+		/* if the conversion specifier is 's' or 'd' return -1 as the precision */
+		if (format[*i + 1] == 's' || format[*i + 1] == 'd')
+				{
+				return (-1);
+				}
+		return (precision);
 
-	*i = curr_i - 1;
-
-	return (precision);
 }
+
